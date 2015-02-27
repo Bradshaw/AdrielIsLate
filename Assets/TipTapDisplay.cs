5218929b677db84aebbd1d876d2f144ac86f8ba0
Kevin Bradshaw <kevin@naturalpad.fr>
2015-02-27 13:42:59 +0100

    TipTap base mechanics are in

---
+++ Assets/TipTapDisplay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TipTapSpeeder))]
+public class TipTapDisplay : MonoBehaviour {
+
+	public Color tip;
+    public Color tap;
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.material.color = (GetComponent<TipTapSpeeder>().current_phase.isTip() ? tip : tap);
+        transform.position = Vector3.left * (GetComponent<TipTapSpeeder>().current_phase.isTip() ? 2 : -2);
+	}
+}
