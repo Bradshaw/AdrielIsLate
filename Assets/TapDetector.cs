db584fca0e94b749546a27e2d9a7bc2e48b5378e
Kevin Bradshaw <kevin@naturalpad.fr>
2015-02-28 14:39:17 -0700

    Jam Final

---
+++ Assets/TapDetector.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapDetector : MonoBehaviour {
+
+    public TipTapSpeeder tts;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var ts = Input.touches;
+        foreach (Touch t in ts)
+        {
+            if (t.phase == TouchPhase.Began)
+            {
+                if (t.position.x > Screen.width / 2)
+                {
+                    tts.doTap();
+                }
+                else
+                {
+                    tts.doTip();
+                }
+
+            }
+        }
+
+	}
+}
